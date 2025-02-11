0356696f1d7c8336659be6abb0a1b433d81889bf
Ryan Harding <141263196+RyaFre@users.noreply.github.com>
2025-02-11 23:57:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 // Global styles are already imported in Layout.js
 function MyApp({ Component, pageProps }) {
